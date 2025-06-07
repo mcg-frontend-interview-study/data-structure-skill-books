ad985865fa748af7a95e9b6cf4bd62eb76fe15db
pakxe <pigkill40@naver.com>
2025-06-07 19:20:32 +0900

    chore: 커버리지 계산 시 타겟 제한

--- jest.config.ts
+++ jest.config.ts
@@ -7,4 +7,11 @@ const presetConfig = createDefaultEsmPreset({
 
 export default {
     ...presetConfig,
+    collectCoverageFrom: [
+        // 커버리지 수집 대상 및 제외 파일. (문법 => ! ? 제외 : 포함)
+        "src/**/*.{ts,tsx,js,jsx}",
+
+        "!src/constants/**",
+        "!src/**/utils/**",
+    ],
 } satisfies Config;
