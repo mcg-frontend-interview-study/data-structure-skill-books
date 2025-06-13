3be3a54a2152028e597d2a6b0cd07d04eab443bb
김진호 <rlawlsgh1227@gmail.com>
2025-06-13 12:55:19 +0900

    chore: prettier 추가 및 설정

--- src/example/Example.ts
+++ src/example/Example.ts
@@ -1,28 +1,28 @@
 import { ActionStatusCode, STATUS_CODE } from "../constants/statusCode";
 
 export class Example {
-    private name: string;
-    private size: number;
+  private name: string;
+  private size: number;
 
-    constructor(name: string) {
-        this.name = name;
-        this.size = 0;
+  constructor(name: string) {
+    this.name = name;
+    this.size = 0;
+  }
+
+  public setSize(size: number): ActionStatusCode {
+    if (size < 0) {
+      return STATUS_CODE.FAIL;
     }
 
-    public setSize(size: number): ActionStatusCode {
-        if (size < 0) {
-            return STATUS_CODE.FAIL;
-        }
+    this.size = size;
+    return STATUS_CODE.SUCCESS;
+  }
 
-        this.size = size;
-        return STATUS_CODE.SUCCESS;
-    }
+  public getSize() {
+    return this.size;
+  }
 
-    public getSize() {
-        return this.size;
-    }
-
-    public getName() {
-        return this.name;
-    }
+  public getName() {
+    return this.name;
+  }
 }
