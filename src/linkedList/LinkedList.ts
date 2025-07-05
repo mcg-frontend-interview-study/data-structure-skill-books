b3d76e246e606f9d4db060f5d38123a5e5f27bce
bmj0053 <bmj05378@gmail.com>
2025-07-06 02:09:00 +0900

    feat: delete 추가

--- src/linkedList/LinkedList.ts
+++ src/linkedList/LinkedList.ts
@@ -65,13 +65,34 @@ export class LinkedList<T> {
       const tempNextNode = currentNode.next;
       currentNode.next = newNode;
       newNode.next = tempNextNode;
-      console.log(this.head);
       return STATUS_CODE.SUCCESS;
     } catch (error) {
       return STATUS_CODE.FAIL;
     }
   }
   // 맨 뒤에 있는 노드 삭제
+  delete(): ActionStatusCode {
+    try {
+      if (this.head === null) {
+        return STATUS_CODE.FAIL;
+      }
+
+      if (this.head.next === null) {
+        this.head = null;
+        return STATUS_CODE.SUCCESS;
+      }
+
+      let currentNode = this.head;
+      while (currentNode.next !== null && currentNode.next.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = null;
+
+      return STATUS_CODE.SUCCESS;
+    } catch (error) {
+      return STATUS_CODE.FAIL;
+    }
+  }
   // 특정 위치에 있는 노드 삭제
   // 특정 값을 갖는 노드 삭제
   // 연결리스트 길이
@@ -80,5 +101,5 @@ export class LinkedList<T> {
 }
 
 // const linkedList = new LinkedList<string>(new LinkedListNode<string>('1'));
-// console.log(linkedList.head?.value);
-// console.log(linkedList.head?.next?.value);
+// console.log(linkedList.head?.next?.next?.value);
+// console.log(linkedList.head?.next?.next?.next?.value);
